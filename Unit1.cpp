8054b8cdff19ee3efceb37d6504215c6f3975641
Ivan123123 <ivanivanbo@mail.ru>
2016-12-05 12:11:09 +0300

    v0.2

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,13 +1,21 @@
-//---------------------------------------------------------------------------
-
-#pragma hdrstop
-
-//---------------------------------------------------------------------------
-
-#pragma argsused
-int main(int argc, char* argv[])
-{
-        return 0;
-}
-//---------------------------------------------------------------------------
+#include <iostream>
+#include <string>
  
+void trim(std::string &str)
+{
+    int pos = str.find_first_not_of(" \t\r\n");
+    str.erase(0, pos);
+    pos = str.find_last_not_of(" \t\r\n");
+    str.erase(pos+1, str.size()-pos);
+}
+ 
+int main()
+{
+   
+
+    std::cout << text << std::endl;
+    getchar();
+
+    return 0;
+}
+
